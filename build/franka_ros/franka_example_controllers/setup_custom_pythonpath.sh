ade7805519d81eb848a7a300277b2877e1466da2
Shubham07-bit <shubhamjolapara256@gmail.com>
2025-02-08 21:15:00 +0530

    Before starting the MuJoCo for Heal Robots for the Redundancy Optmization. Cleaning some of the old codes.

--- build/franka_ros/franka_example_controllers/setup_custom_pythonpath.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/iitgn-robotics/franka_dual_arm_ws/devel/lib/python3/dist-packages:$PYTHONPATH
-exec /usr/bin/python3 "$@"
